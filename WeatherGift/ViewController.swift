2be10bc40c2a024cb708f6c3f3fa7de91ad5e80d
Jess Newman <jessnewman@jesss-mbp.bc.edu>
2018-03-14 13:02:34 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Jess Newman on 3/14/18.
+//  Copyright © 2018 Newman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
